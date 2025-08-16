0b1615d7071714c786ca454c8676c98b6c69559d
Laxmi Mahato <laxmi.mahato.lm.9803@gmail.com>
2025-08-16 23:22:27 +0545

    add bar chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import AppBarChart from "@/components/AppBarChart";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="p-4 bg-primary-foreground rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="p-4 bg-primary-foreground rounded-lg">Test</div>
+      <div className="p-4 bg-primary-foreground rounded-lg">Test</div>
+      <div className="p-4 bg-primary-foreground rounded-lg">Test</div>
+      <div className="p-4 bg-primary-foreground rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test
+      </div>
+      <div className="p-4 bg-primary-foreground rounded-lg">Test</div>
+    </div>
+  );
 }
